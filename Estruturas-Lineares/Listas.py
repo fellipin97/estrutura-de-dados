84208aa85bafcafdc770824fb44dede6eaf4d5db
GustavoRTC7 <gustavinrtc@gmail.com>
2025-03-17 01:56:15 -0300

    Update and rename Listas to Listas.py

---
+++ Estruturas-Lineares/Listas.py
@@ -0,0 +1,11 @@
+# Exemplo de uma lista em Python
+
+lista = [1, 2, 3, 4, 5]
+lista.append(6)  # Inserção de elemento no final
+print(lista)  # [1, 2, 3, 4, 5, 6]
+
+lista.remove(3)  # Remoção do elemento 3
+print(lista)  # [1, 2, 4, 5, 6]
+
+print(lista[2])  # Acesso ao elemento na posição 2 (4)
+
